08b60f7fc8d1176cd7dadf8ae9fff94b924baf23
Yauheni Aliakseyenka <alexeenko.e.p@gmail.com>
2023-07-22 17:34:24 +0300

    init

---
+++ src/Razor.Emails.API/Controllers/Emails.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Razor.Emails.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Emails : ControllerBase
+	{
+		[Route("preview")]
+		[HttpGet]
+		public IActionResult GetPreview()
+		{
+			return new ContentResult();
+		}
+	}
+}
